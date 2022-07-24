0306b0710220808ae01fbac320c45b401d2c93b8
Asad Anwar <asadanwar60@gmail.com>
2022-07-24 10:22:28 +0530

    Made an event viewer with fetch api,dynamic- router and vuex

---
+++ src/views/EventDetails.vue
@@ -0,0 +1,29 @@
+<template>
+  <div v-if="event">
+    <h4>{{ event.title }}</h4>
+    <span>@{{ event.time }} on {{ event.date }} @ {{ event.location }}</span>
+    <p>{{ event.description }}</p>
+  </div>
+</template>
+<script>
+import EventService from "@/services/EventService";
+
+export default {
+  props: ["id"],
+  data() {
+    return {
+      event: null,
+    };
+  },
+  created() {
+    EventService.getEvent(this.id)
+      .then((response) => {
+        // console.log("events", response.data);
+        this.event = response.data;
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  },
+};
+</script>
